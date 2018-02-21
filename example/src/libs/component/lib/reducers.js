420c0f1da87b8c11ddb9a5bc26b5dc750d2da8e6
mjbp <mick.perkins@stormid.com>
2018-02-21 17:23:56 +0000

    Update 2018-02-21T17:23:49.803Z

--- example/src/libs/component/lib/reducers.js
+++
@@ -1,5 +0,0 @@
-export default {
-    setInitialState: (state, data) => Object.assign({}, state, data),
-    addInput: (state, data) => Object.assign({}, state, { clones: [...state.clones, data]}),
-    deleteInput: (state, data) => Object.assign({}, state, { clones: state.clones.filter(clone => clone.button !== data.button)})
-};
--- src/lib/constants.js
+++
@@ -1,5 +0,0 @@
-export const KEY_CODES = {
-    ENTER: 13
-};
-
-export const TRIGGER_EVENTS = ['click', 'keydown' ];
